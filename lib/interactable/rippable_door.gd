ea36d3aad503eaadda59dee97e43f62b6df4810f
juizoi <thomasmouilleron@gmail.com>
2025-06-01 00:32:42 +0100

    merge fix

--- lib/interactable/rippable_door.gd
+++ lib/interactable/rippable_door.gd
@@ -9,7 +9,7 @@ var no_scissors_dialogue: DialogueResource = preload("res://dialogues/no_scissor
 
 func _ready() -> void:
 	super._ready()
-	player_og_quat = Quaternion(%PlayerChildMesh.global_basis)
+	player_og_quat = Quaternion(%SK_PlayerCharacter.global_basis)
 
 
 func _on_interacted() -> void:
@@ -20,14 +20,16 @@ func _on_interacted() -> void:
 
 	if can_loot and Global.Ability.SCISSORS in can_loot.inventory:
 		animation_player.play("CutOpen")
-		
+
 		# TODO Play walking forward anim but facing backwards to the audience
 		get_tree().create_timer(1.0).timeout.connect(curtains_fall)
 	else:
 		Global.narrator_bubble.show_dialogue(no_scissors_dialogue)
 
+
 func curtains_fall() -> void:
 	pass
+
 
 func move_to_door() -> PropertyTweener:
 	var stage_body_glob_pos := Global.stage.stage_body.global_position
@@ -46,4 +48,4 @@ func rotate_to_door() -> MethodTweener:
 		.tween_method(
 			func(delta: float) -> void:
 				var b := Basis(player_og_quat.slerp(Quaternion(Vector3.UP, PI), delta))
-				%PlayerChildMesh.global_basis = b, 0.0, 1.0, 1.0)
+				%SK_PlayerCharacter.global_basis = b, 0.0, 1.0, 1.0)

3c5a86d80e6944b25066fe8de8e123aa843b6723
juizoi <thomasmouilleron@gmail.com>
2025-06-03 12:58:40 +0100

    refactored platform falling

--- lib/components/can_reset.gd
+++ lib/components/can_reset.gd
@@ -4,14 +4,11 @@ extends Component
 @export var animation_player: AnimationPlayer
 var parent: FallingPlatform
 
-var resetting_object := false
-
 
 func _node_ready() -> void:
 	parent = get_object() as FallingPlatform
 
 
 func reset() -> void:
-	resetting_object = true
 	animation_player.play("RESET")
 	parent.reset_platform()
--- scenes/platform/falling_platform.gd
+++ scenes/platform/falling_platform.gd
@@ -6,23 +6,19 @@ class_name FallingPlatform
 @export var can_reset: CanReset
 @export var platform_animation: AnimationPlayer
 
-var fallen: bool = false
+var is_falling: bool = false
 
 
 # When the player steps on the plaform
 func _on_hit_detection_body_entered(body: Node3D) -> void:
-	if body is Character and not can_reset.resetting_object:
+	if body is Character and not is_falling:
+		is_falling = true
 		platform_animation.play("fall")
 
-
-# Overriding this is the godot way of **properly** changing a rigid body's position
-func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	if can_reset.resetting_object:
-		reset_platform()
 
 func reset_platform() -> void:
 	get_tree()\
 		.create_tween()\
 		.tween_property(self, "global_position", original_object_pos, .8)\
 		.set_trans(Tween.TRANS_SINE)\
-		.finished.connect(func() -> void: can_reset.resetting_object = false)
+		.finished.connect(func() -> void: is_falling = false)

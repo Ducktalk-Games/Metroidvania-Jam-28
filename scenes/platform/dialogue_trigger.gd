39fd27c073f7c0e6fd3359ed821e0fdb51fab023
juizoi <thomasmouilleron@gmail.com>
2025-06-04 14:44:26 +0100

    simple dialogue triggers are now spotlight dialogue triggers

--- scenes/platform/dialogue_trigger.gd
+++ scenes/platform/dialogue_trigger.gd
@@ -10,7 +10,22 @@ extends Area3D
 
 		label_string = value
 
+var triggered: bool
+
+signal stage_body_entered(body: StageCharacter)
+
 
 func _ready() -> void:
 	if Engine.is_editor_hint(): return
 	trigger_label.hide()
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if Engine.is_editor_hint(): return
+	if body is StageCharacter:
+		_stage_body_entered(body)
+		triggered = true
+
+
+func _stage_body_entered(body: StageCharacter) -> void:
+	pass

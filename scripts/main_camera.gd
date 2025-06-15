f0697ffc571d0153d71507986551debe386d8b4a
juizoi <thomasmouilleron@gmail.com>
2025-06-15 22:27:36 +0100

    added a bunch of changes

--- scripts/main_camera.gd
+++ scripts/main_camera.gd
@@ -5,11 +5,16 @@ extends Camera3D
 @onready var camera_animation_player: AnimationPlayer = %CameraAnimationPlayer
 @onready var main_menu: MainMenu = %MainMenu
 
+@export var camera_audio: AudioStreamPlayer
+
+@export var options_menu: OptionsAmp
+
 signal pivoted_to_parent_menu
 
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
+		camera_audio.play()
 		match Global.current_menu_state:
 			Global.MenuState.CREDITS:
 				pivot_from_credits()
@@ -30,6 +35,7 @@ func pivot_to_options(ingame: bool = false) -> void:
 		Global.current_menu_state = Global.MenuState.OPTIONS
 
 	camera_animation_player.play("pivot_to_options")
+	get_tree().create_tween().tween_property(options_menu.options_audio_stream, "volume_db", 0.0, 0.5)
 
 
 func pivot_from_options(ingame: bool = false) -> void:
@@ -40,9 +46,10 @@ func pivot_from_options(ingame: bool = false) -> void:
 		Global.current_menu_state = Global.current_parent_menu_state
 
 	Global.disable_options_menu()
-
 	camera_animation_player.play("pivot_to_options", -1, -1, true)
 	wait_for_from_anim_to_finish()
+
+	get_tree().create_tween().tween_property(options_menu.options_audio_stream, "volume_db", -20.0, 0.5).finished
 
 
 func pivot_to_credits() -> void:

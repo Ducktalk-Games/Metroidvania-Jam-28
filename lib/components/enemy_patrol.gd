c0a7129fdb2451687483208aaeb696abdcfc2d83
juizoi <thomasmouilleron@gmail.com>
2025-06-15 23:29:56 +0100

    patrol implemented for enemy

--- lib/components/enemy_patrol.gd
+++ lib/components/enemy_patrol.gd
@@ -7,19 +7,9 @@ var patrol_speed: float = 10.0
 
 @export
 var end_of_path_delay: float = 0.1
-#
-#@onready
-#var points_node: Node3D = $PatrollingPoints
 
 @onready
 var patrol_agent := get_object() as CharacterBody3D
-
-@onready
-var patrolling:= false
-
-var is_waiting: bool
-
-var current_path: String
 
 @export
 var point_a: Node3D
@@ -28,74 +18,13 @@ var point_a: Node3D
 var point_b: Node3D
 
 
-func toggle_patrolling() -> void:
-	patrolling = !patrolling
-
-
 func _ready() -> void:
-	is_waiting = false
-	current_path = point_a.name
-	#toggle_patrolling()
-	pass
 
+	var point_a_pos := point_a.global_position
+	var point_b_pos := point_b.global_position
 
-func patrol(delta: float) -> void:
-	var direction_vector: Vector3
-	var can_move: bool = !patrol_agent.is_on_wall()
-	var distance: float
-
-	match current_path:
-		point_a.name:
-			direction_vector = (point_a.global_position - patrol_agent.global_position).normalized()
-			distance = patrol_agent.global_position.x - point_a.global_position.x
-
-			#Set the direction vector to 0 and switch direction if we are close to the edge of point a
-			if distance < 1 || !can_move:
-				wait(end_of_path_delay)
-				current_path = point_b.name
-
-		point_b.name:
-			direction_vector = (point_b.global_position - patrol_agent.global_position).normalized()
-			distance = patrol_agent.global_position.x - point_b.global_position.x
-
-			if distance < 1 || !can_move:
-				#TODO:
-				wait(end_of_path_delay)
-				current_path = point_a.name
-
-	#character.velocity.y += character.get_gravity().y * delta
-	# Move the character by its velocity
-	if !is_waiting:
-		#Start movement
-		patrol_agent.velocity.x = direction_vector.x * patrol_speed
-		patrol_agent.move_and_slide()
-
-
-func wait(seconds: float) -> void:
-	is_waiting = true
-	await get_tree().create_timer(end_of_path_delay, true, false).timeout
-	is_waiting = false
-	pass
-
-
-func _physics_process(delta: float) -> void:
-	if (patrolling):
-		patrol(delta)
-
-
-func _on_reach_patrol_point() -> void:
-	is_waiting = true
-	#patrol_speed = Vector3.ZERO
-	#move_and_slide()
-	#await get_tree().create_timer(wait_time).timeout
-	#current_target_idx = (current_target_idx + 1) % patrol_points.size()
-	#_set_new_path()
-	is_waiting = false
-
-
-func _set_new_path() -> void:
-	pass
-	#var start = global_position
-	#var end = patrol_points[current_target_idx]
-	#path = nav.map_get_path(nav_map, start, end, false)
-	#path_index = 0
+	var patrol_tween: Tween = get_tree().create_tween().set_loops()
+	patrol_tween.tween_property(patrol_agent, "global_position", point_b_pos, patrol_speed)
+	patrol_tween.tween_property(patrol_agent, "global_rotation", Vector3.UP * PI, end_of_path_delay).as_relative()
+	patrol_tween.tween_property(patrol_agent, "global_position", point_a_pos, patrol_speed)
+	patrol_tween.tween_property(patrol_agent, "global_rotation", Vector3.UP * PI, end_of_path_delay).as_relative()

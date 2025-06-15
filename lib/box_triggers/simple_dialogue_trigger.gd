422fd18f33c5689b8eb02543dc57a2d8804cefb1
juizoi <thomasmouilleron@gmail.com>
2025-06-15 18:26:55 +0100

    lion dialogue triggers on house level start

--- lib/box_triggers/simple_dialogue_trigger.gd
+++ lib/box_triggers/simple_dialogue_trigger.gd
@@ -5,8 +5,12 @@ extends BoxTrigger
 @export var dialogue_resource: DialogueResource
 
 
-func _stage_body_entered(body: StageCharacter) -> void:
+func play_triggered_dialogue() -> void:
 	assert(dialogue_resource, "No Dialogue resource found in " + name)
 	if not triggered:
 		DialogueSequencer.start_dialog(dialogue_resource.resource_path)
 		triggered = true
+
+
+func _stage_body_entered(body: StageCharacter) -> void:
+	play_triggered_dialogue()
--- scenes/platform/house_level.gd
+++ scenes/platform/house_level.gd
@@ -1,6 +1,7 @@
 extends PlatformLevel
 
 @export var PlayerStart: Marker3D
+@export var player_finds_lion_trigger: SimpleDialogueTrigger
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +13,9 @@ func _ready() -> void:
 	(Component.find(character, "CanMove") as CanMove).speed *= 0.7
 	(Component.find(character, "CanJump") as CanJump).disable()
 	(Component.find(character, "CanLoot") as CanLoot).inventory.clear()
+
+	await Global.curtains_opened
+	player_finds_lion_trigger.play_triggered_dialogue()
 
 	await DialogueManager.dialogue_ended
 	Global.stage.patron.set_music_to("house_theme")
